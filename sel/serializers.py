2f7e0846f3f00e0c39ed49ebc526e97c35d0f193
David Macro <david.macro7@gmail.com>
2017-10-06 11:07:52 -0700

    json: can serialize uuid fields.

--- sel/serializers.py
+++ sel/serializers.py
@@ -1,9 +1,12 @@
 #-*- coding:utf-8 -*-
 import datetime
 import json
+import uuid
 
 class JsonEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, datetime.datetime):
             return o.isoformat()
+        elif isinstance(o, uuid.UUID):
+            return o.hex
         return json.JSONEncoder.default(self, o)
